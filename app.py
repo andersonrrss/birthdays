ac7cc6ffb98cda9c5f77c3bb8cc2660b13fe17e3
andersonrrss <andersonrss122@hotmail.com>
2024-04-08 22:32:26 -0300

    Tratando erros de input do código

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
@@ -23,6 +24,34 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        with sqlite3.connect("birthdays.db") as conn:
+            db = conn.cursor()
+            name = request.form.get("name")
+            month = request.form.get("month")
+            day = request.form.get("day")
+            # Checa se as informações foram submetidas
+            if not name:
+                return render_template("error.html", message="Type a name")
+            if not month:
+                return render_template("error.html", message="Type a month")
+            if not day:
+                return render_template("error.html", message="Type a day")
+            # Checa se o dia e o mês são válidos
+            try:
+                month = int(month)
+                if month > 12 or month < 1:
+                    return render_template("error.html", message="Type a valid month")
+            except:
+                return render_template("error.html", message="Type the number of the month")
+
+            try:
+                day = int(day)
+                if day > 31 or day < 1:
+                    return render_template("error.html", message="Type a valid day" )
+            except:
+                return render_template("error.html", message="Type a number of the day")
+
+            db.execute("INSERT INTO birthdays (name,month,day) VALUES (?,?,?)", (name.capitalize(),month,day))
 
         return redirect("/")
 
@@ -30,7 +59,18 @@ def index():
 
         # TODO: Display the entries in the database on index.html
 
-        return render_template("index.html")
+        with sqlite3.connect("birthdays.db") as conn:
+            db = conn.cursor()
+            data = db.execute("SELECT * FROM birthdays").fetchall()
+            birthdays = []
+            for birthday in data:
+                bruna = {
+                    "name": birthday[1],
+                    "date": f"{birthday[2]}/{birthday[3]}"
+                }
+                birthdays.append(bruna)
+
+        return render_template("index.html", birthdays=birthdays)
 
 if __name__ == "__main__":
     app.run()
